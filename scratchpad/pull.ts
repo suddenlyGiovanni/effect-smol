4a1763ec926d168c14f4294ae1fc3c7019f947e3
Tim <hello@timsmart.co>
2024-12-19 16:38:16 +1300

    add pull scratchpad

---
+++ scratchpad/pull.ts
@@ -0,0 +1,45 @@
+import * as Channel from "effect/Channel"
+import * as Effect from "effect/Effect"
+
+const abc = Channel.asyncPush<string>(
+  Effect.fnUntraced(function* (emit) {
+    yield* Effect.addFinalizer(() =>
+      Effect.sync(() => console.log("finalizer")),
+    )
+    emit.single("a")
+    emit.single("b")
+    emit.single("c")
+    emit.end()
+  }),
+)
+
+const merged = Channel.mergeAll({ concurrency: 2 })(
+  Channel.fromIterable([abc, abc, abc, abc, abc]),
+)
+
+Effect.gen(function* () {
+  const pull = yield* Channel.toPull(merged)
+  console.log(yield* pull)
+  console.log(yield* pull)
+  console.log(yield* pull)
+
+  console.log(yield* pull)
+  console.log(yield* pull)
+  console.log(yield* pull)
+
+  console.log(yield* pull)
+  console.log(yield* pull)
+  console.log(yield* pull)
+
+  console.log(yield* pull)
+  console.log(yield* pull)
+  console.log(yield* pull)
+
+  console.log(yield* pull)
+  console.log(yield* pull)
+  console.log(yield* pull)
+
+  yield* pull.pipe(
+    Effect.catch((error) => Effect.sync(() => console.log("error", error))),
+  )
+}).pipe(Effect.scoped, Effect.runPromise)
