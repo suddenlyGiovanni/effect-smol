066ee0da9e2cbcbda0a598ee9cc1bc0be9beeabb
Giulio Canti <giulio.canti@gmail.com>
2025-02-05 14:32:38 +0100

    expose internal Effect.either (#46)

--- src/Effect.ts
+++ src/Effect.ts
@@ -1492,6 +1492,76 @@ export const tap: {
 } = internal.tap
 
 /**
+ * Encapsulates both success and failure of an `Effect` into an `Either` type.
+ *
+ * **Details**
+ *
+ * This function converts an effect that may fail into an effect that always
+ * succeeds, wrapping the outcome in an `Either` type. The result will be
+ * `Either.Left` if the effect fails, containing the recoverable error, or
+ * `Either.Right` if it succeeds, containing the result.
+ *
+ * Using this function, you can handle recoverable errors explicitly without
+ * causing the effect to fail. This is particularly useful in scenarios where
+ * you want to chain effects and manage both success and failure in the same
+ * logical flow.
+ *
+ * It's important to note that unrecoverable errors, often referred to as
+ * "defects," are still thrown and not captured within the `Either` type. Only
+ * failures that are explicitly represented as recoverable errors in the effect
+ * are encapsulated.
+ *
+ * The resulting effect cannot fail directly because all recoverable failures
+ * are represented inside the `Either` type.
+ *
+ * @see {@link option} for a version that uses `Option` instead.
+ * @see {@link exit} for a version that encapsulates both recoverable errors and defects in an `Exit`.
+ *
+ * @example
+ * ```ts
+ * import { Effect, Either, Random } from "effect"
+ *
+ * class HttpError {
+ *   readonly _tag = "HttpError"
+ * }
+ *
+ * class ValidationError {
+ *   readonly _tag = "ValidationError"
+ * }
+ *
+ * //      ┌─── Effect<string, HttpError | ValidationError, never>
+ * //      ▼
+ * const program = Effect.gen(function* () {
+ *   const n1 = yield* Random.next
+ *   const n2 = yield* Random.next
+ *   if (n1 < 0.5) {
+ *     yield* Effect.fail(new HttpError())
+ *   }
+ *   if (n2 < 0.5) {
+ *     yield* Effect.fail(new ValidationError())
+ *   }
+ *   return "some result"
+ * })
+ *
+ * //      ┌─── Effect<string, never, never>
+ * //      ▼
+ * const recovered = Effect.gen(function* () {
+ *   //      ┌─── Either<string, HttpError | ValidationError>
+ *   //      ▼
+ *   const failureOrSuccess = yield* Effect.either(program)
+ *   return Either.match(failureOrSuccess, {
+ *     onLeft: (error) => `Recovering from ${error._tag}`,
+ *     onRight: (value) => value // Do nothing in case of success
+ *   })
+ * })
+ * ```
+ *
+ * @since 2.0.0
+ * @category Outcome Encapsulation
+ */
+export const either: <A, E, R>(self: Effect<A, E, R>) => Effect<Either<A, E>, never, R> = internal.either
+
+/**
  * Transforms an effect to encapsulate both failure and success using the `Exit`
  * data type.
  *
