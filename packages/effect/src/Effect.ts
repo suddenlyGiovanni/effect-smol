106187551a6607835ba680d903ee329b21e13525
Tim <hello@timsmart.co>
2025-06-28 21:04:28 +1200

    add Effect type constraint apis (#189)

--- packages/effect/src/Effect.ts
+++ packages/effect/src/Effect.ts
@@ -6076,3 +6076,25 @@ export const effectify: {
         resume(onSyncError ? fail(onSyncError(err, args)) : die(err))
       }
     })) as any
+
+// -----------------------------------------------------------------------------
+// Type constraints
+// -----------------------------------------------------------------------------
+
+/**
+ * @since 4.0.0
+ * @category Type constraints
+ */
+export const ensureSuccessType = <A>() => <A2 extends A, E, R>(effect: Effect<A2, E, R>): Effect<A2, E, R> => effect
+
+/**
+ * @since 4.0.0
+ * @category Type constraints
+ */
+export const ensureErrorType = <E>() => <A, E2 extends E, R>(effect: Effect<A, E2, R>): Effect<A, E2, R> => effect
+
+/**
+ * @since 4.0.0
+ * @category Type constraints
+ */
+export const ensureRequirementsType = <R>() => <A, E, R2 extends R>(effect: Effect<A, E, R2>): Effect<A, E, R2> => effect
