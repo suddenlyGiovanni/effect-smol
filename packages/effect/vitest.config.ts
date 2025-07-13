19cd3d7c093f6a0fe66dc7ad516d95f92880b469
Sebastian Lorenz <fubhy@fubhy.com>
2025-07-13 09:54:42 +0200

    remove leftover/invalid custom vitest config (#277)

--- packages/effect/vitest.config.ts
+++ packages/effect/vitest.config.ts
@@ -1,13 +1,6 @@
 import { mergeConfig, type ViteUserConfig } from "vitest/config"
 import shared from "../../vitest.shared.js"
 
-const config: ViteUserConfig = {
-  test: {
-    coverage: {
-      reporter: ["html"],
-      include: ["src/Schema.ts"]
-    }
-  }
-}
+const config: ViteUserConfig = {}
 
 export default mergeConfig(shared, config)
