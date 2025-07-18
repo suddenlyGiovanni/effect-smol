539449b87f089d186f9843f5c94a30ddace81d37
Sebastian Lorenz <fubhy@fubhy.com>
2025-07-18 09:55:39 +0200

    prepare for testing with `bun` (#328)

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vitest/config"
 
 const isDeno = process.versions.deno !== undefined
+const isBun = process.versions.bun !== undefined
 
 export default defineConfig({
   test: {
@@ -8,9 +9,14 @@ export default defineConfig({
       "packages/*/vitest.config.ts",
       ...(isDeno ?
         [
-          "!./packages/platform-bun",
-          "!./packages/platform-node",
-          "!./packages/platform-node-shared"
+          "!packages/platform-bun",
+          "!packages/platform-node",
+          "!packages/platform-node-shared"
+        ] :
+        []),
+      ...(isBun ?
+        [
+          "!packages/platform-node"
         ] :
         [])
     ]
