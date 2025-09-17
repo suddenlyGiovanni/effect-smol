a0637696c4418fa7d0cec1e720fb4c7637406c4d
Giulio Canti <giulio.canti@gmail.com>
2025-09-17 11:10:23 +0200

    update schema benchmarks to use Schema APIs (#532)

--- packages/effect/benchmark/schema/Enums.ts
+++ packages/effect/benchmark/schema/Enums.ts
@@ -1,12 +1,12 @@
-import { Schema, ToParser } from "effect/schema"
+import { Schema } from "effect/schema"
 import { Bench } from "tinybench"
 
 /*
 ┌─────────┬───────────┬──────────────────┬──────────────────┬────────────────────────┬────────────────────────┬──────────┐
 │ (index) │ Task name │ Latency avg (ns) │ Latency med (ns) │ Throughput avg (ops/s) │ Throughput med (ops/s) │ Samples  │
 ├─────────┼───────────┼──────────────────┼──────────────────┼────────────────────────┼────────────────────────┼──────────┤
-│ 0       │ 'valid'   │ '75.06 ± 0.79%'  │ '83.00 ± 1.00'   │ '14894430 ± 0.02%'     │ '12048193 ± 143431'    │ 13323362 │
-│ 1       │ 'invalid' │ '229.27 ± 4.05%' │ '166.00 ± 41.00' │ '6660351 ± 0.02%'      │ '6024096 ± 1216404'    │ 4361644  │
+│ 0       │ 'good'    │ '79.26 ± 0.73%'  │ '83.00 ± 1.00'   │ '14443802 ± 0.02%'     │ '12048193 ± 143431'    │ 12616336 │
+│ 1       │ 'bad'     │ '131.52 ± 0.92%' │ '125.00 ± 0.00'  │ '7976946 ± 0.01%'      │ '8000000 ± 0'          │ 7603261  │
 └─────────┴───────────┴──────────────────┴──────────────────┴────────────────────────┴────────────────────────┴──────────┘
 */
 
@@ -19,20 +19,20 @@ enum Enum {
 
 const schema = Schema.Enums(Enum)
 
-const valid = "b"
-const invalid = "c"
+const good = "b"
+const bad = "c"
 
-const decodeUnknownExit = ToParser.decodeUnknownExit(schema)
+const decodeUnknownExit = Schema.decodeUnknownExit(schema)
 
 // console.log(decodeUnknownExit(valid))
 // console.log(decodeUnknownExit(invalid))
 
 bench
-  .add("valid", function() {
-    decodeUnknownExit(valid)
+  .add("good", function() {
+    decodeUnknownExit(good)
   })
-  .add("invalid", function() {
-    decodeUnknownExit(invalid)
+  .add("bad", function() {
+    decodeUnknownExit(bad)
   })
 
 await bench.run()
--- packages/effect/benchmark/schema/tagged-union.ts
+++ packages/effect/benchmark/schema/tagged-union.ts
@@ -1,29 +1,33 @@
 import { Array as RA } from "effect/collections"
-import type { AST } from "effect/schema"
-import { Schema, ToParser } from "effect/schema"
+import { Schema } from "effect/schema"
 import { Bench } from "tinybench"
 
 /*
-┌─────────┬─────────────────┬──────────────────┬──────────────────┬────────────────────────┬────────────────────────┬─────────┐
-│ (index) │ Task name       │ Latency avg (ns) │ Latency med (ns) │ Throughput avg (ops/s) │ Throughput med (ops/s) │ Samples │
-├─────────┼─────────────────┼──────────────────┼──────────────────┼────────────────────────┼────────────────────────┼─────────┤
-│ 0       │ 'Schema (good)' │ '492.49 ± 0.48%' │ '458.00 ± 1.00'  │ '2116578 ± 0.01%'      │ '2183406 ± 4757'       │ 2030480 │
-│ 1       │ 'Schema (bad)'  │ '1135.7 ± 3.93%' │ '791.00 ± 41.00' │ '1280031 ± 0.02%'      │ '1264223 ± 69111'      │ 880517  │
-└─────────┴─────────────────┴──────────────────┴──────────────────┴────────────────────────┴────────────────────────┴─────────┘
+┌─────────┬────────────────────┬──────────────────┬──────────────────┬────────────────────────┬────────────────────────┬─────────┐
+│ (index) │ Task name          │ Latency avg (ns) │ Latency med (ns) │ Throughput avg (ops/s) │ Throughput med (ops/s) │ Samples │
+├─────────┼────────────────────┼──────────────────┼──────────────────┼────────────────────────┼────────────────────────┼─────────┤
+│ 0       │ 'Schema (good)'    │ '488.43 ± 0.48%' │ '459.00 ± 1.00'  │ '2119495 ± 0.01%'      │ '2178649 ± 4757'       │ 2047396 │
+│ 1       │ 'Schema (bad)'     │ '629.17 ± 0.29%' │ '584.00 ± 1.00'  │ '1645689 ± 0.01%'      │ '1712329 ± 2937'       │ 1589395 │
+│ 2       │ 'candidate (good)' │ '327.20 ± 0.27%' │ '292.00 ± 1.00'  │ '3205291 ± 0.01%'      │ '3424658 ± 11769'      │ 3056264 │
+│ 3       │ 'candidate (bad)'  │ '449.52 ± 2.41%' │ '417.00 ± 0.00'  │ '2372897 ± 0.01%'      │ '2398082 ± 0'          │ 2224610 │
+└─────────┴────────────────────┴──────────────────┴──────────────────┴────────────────────────┴────────────────────────┴─────────┘
 */
 
 const bench = new Bench({ time: 1000 })
 
 const n = 100
-const members = RA.makeBy(n, (i) =>
+const f = (i: number) =>
   Schema.Struct({
     kind: Schema.Literal(i),
     a: Schema.String,
     b: Schema.Number,
     c: Schema.Boolean
-  }))
+  })
+const members = RA.makeBy(n, f)
 
 const schema = Schema.Union(members)
+
+const candidate = f(n - 1)
 
 const good = {
   kind: n - 1,
@@ -39,18 +43,26 @@ const bad = {
   c: "c"
 }
 
-const decodeUnknownExit = ToParser.decodeUnknownExit(schema)
-const options: AST.ParseOptions = { errors: "all" }
+const decodeUnknownExit = Schema.decodeUnknownExit(schema)
+const decodeUnknownExitCandidate = Schema.decodeUnknownExit(candidate)
 
 // console.log(decodeUnknownExit(good))
 // console.log(decodeUnknownExit(bad))
+// console.log(decodeUnknownExitCandidate(good))
+// console.log(decodeUnknownExitCandidate(bad))
 
 bench
   .add("Schema (good)", function() {
-    decodeUnknownExit(good, options)
+    decodeUnknownExit(good)
   })
   .add("Schema (bad)", function() {
-    decodeUnknownExit(bad, options)
+    decodeUnknownExit(bad)
+  })
+  .add("candidate (good)", function() {
+    decodeUnknownExitCandidate(good)
+  })
+  .add("candidate (bad)", function() {
+    decodeUnknownExitCandidate(bad)
   })
 
 await bench.run()
