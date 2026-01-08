eadfab5e2ac66861ee058bb7546c768e3b6c1417
Sebastian Lorenz <fubhy@fubhy.com>
2026-01-08 19:44:11 +0100

    chore: remove tsup (#890)

--- packages/tools/openapi-generator/src/bin.ts
+++ packages/tools/openapi-generator/src/bin.ts
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 import * as NodeRuntime from "@effect/platform-node/NodeRuntime"
 import * as NodeServices from "@effect/platform-node/NodeServices"
 import * as Effect from "effect/Effect"
