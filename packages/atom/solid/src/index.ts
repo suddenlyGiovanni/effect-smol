8d30169acca94ff19d688deb56ab80c5725c8d8b
Tim Smart <hello@timsmart.co>
2026-02-03 12:39:58 +1300

    remove solid re-exports

--- packages/atom/solid/src/index.ts
+++ packages/atom/solid/src/index.ts
@@ -4,48 +4,6 @@
 
 /**
  * @since 1.0.0
- * @category re-exports
- */
-export * as AsyncResult from "effect/unstable/reactivity/AsyncResult"
-
-/**
- * @since 1.0.0
- * @category re-exports
- */
-export * as Atom from "effect/unstable/reactivity/Atom"
-
-/**
- * @since 1.0.0
- * @category re-exports
- */
-export * as AtomHttpApi from "effect/unstable/reactivity/AtomHttpApi"
-
-/**
- * @since 1.0.0
- * @category re-exports
- */
-export * as AtomRef from "effect/unstable/reactivity/AtomRef"
-
-/**
- * @since 1.0.0
- * @category re-exports
- */
-export * as AtomRegistry from "effect/unstable/reactivity/AtomRegistry"
-
-/**
- * @since 1.0.0
- * @category re-exports
- */
-export * as AtomRpc from "effect/unstable/reactivity/AtomRpc"
-
-/**
- * @since 1.0.0
- * @category re-exports
- */
-export * as Reactivity from "effect/unstable/reactivity/Reactivity"
-
-/**
- * @since 1.0.0
  */
 export * from "./Hooks.ts"
 
