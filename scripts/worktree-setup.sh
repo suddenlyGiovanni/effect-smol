f1f63f39768b1c45357f2ac672c08537249d482a
Tim Smart <hello@timsmart.co>
2026-02-03 09:40:32 +1300

    update worktree-setup

--- scripts/worktree-setup.sh
+++ scripts/worktree-setup.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# reset to latest main
-git fetch origin
-git checkout origin/main
-
 # install dependencies
 direnv allow
 corepack install
@@ -15,12 +11,14 @@ git clone --depth 1 https://github.com/tim-smart/effect-atom.git .repos/effect-a
 
 cat << EOF >> AGENTS.md
 
-## References
+## Learning about "effect" v3
 
 If you need to learn more about the old version of effect (version 3.x), you can
 access the archived repository here:
 
 \`.repos/effect-old\`
+
+## Learning about "effect-atom" old version
 
 If you need to learn more about the old version of effect atom, you can
 access the archived repository here:
