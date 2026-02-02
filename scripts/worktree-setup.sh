c815db93b99f69349697203e72dff4b4104921bb
Tim Smart <hello@timsmart.co>
2026-02-03 11:18:40 +1300

    remove atom from worktree-setup

--- scripts/worktree-setup.sh
+++ scripts/worktree-setup.sh
@@ -7,7 +7,6 @@ pnpm install
 
 # setup repositories
 git clone --depth 1 https://github.com/effect-ts/effect.git .repos/effect-old
-git clone --depth 1 https://github.com/tim-smart/effect-atom.git .repos/effect-atom-old
 
 cat << EOF >> AGENTS.md
 
@@ -17,11 +16,4 @@ If you need to learn more about the old version of effect (version 3.x), you can
 access the archived repository here:
 
 \`.repos/effect-old\`
-
-## Learning more about "effect-atom"
-
-If you need to learn more about the old version of effect atom, you can
-access the archived repository here:
-
-\`.repos/effect-atom-old\`
 EOF
