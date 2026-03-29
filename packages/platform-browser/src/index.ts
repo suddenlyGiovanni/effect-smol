583ea002fdccc58fd2110a36c6d103e63152dcb3
Sandro Maglione <lass.maglio@gmail.com>
2026-03-29 14:04:08 +0200

    Added modules for IndexedDb (#1240)
    Co-authored-by: Tim Smart <hello@timsmart.co>

--- packages/platform-browser/src/index.ts
+++ packages/platform-browser/src/index.ts
@@ -50,6 +50,31 @@ export * as Clipboard from "./Clipboard.ts"
 export * as Geolocation from "./Geolocation.ts"
 
 /**
+ * @since 4.0.0
+ */
+export * as IndexedDb from "./IndexedDb.ts"
+
+/**
+ * @since 4.0.0
+ */
+export * as IndexedDbDatabase from "./IndexedDbDatabase.ts"
+
+/**
+ * @since 4.0.0
+ */
+export * as IndexedDbQueryBuilder from "./IndexedDbQueryBuilder.ts"
+
+/**
+ * @since 4.0.0
+ */
+export * as IndexedDbTable from "./IndexedDbTable.ts"
+
+/**
+ * @since 4.0.0
+ */
+export * as IndexedDbVersion from "./IndexedDbVersion.ts"
+
+/**
  * @since 1.0.0
  */
 export * as Permissions from "./Permissions.ts"
